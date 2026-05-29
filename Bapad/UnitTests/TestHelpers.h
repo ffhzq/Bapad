7af6221bc1738c33d91b32c960868334df59cc32
zq <you@example.com>
2026-05-29 16:27:24 +0800

    fix(test): add char16_t ostream operator for gtest 1.8.1 C++20 compat
    gtest 1.8.1 internally calls *os << char16_t when printing values,
    but C++20 deleted the built-in ostream operator<< for char16_t.
    Provide a global overload that prints as int.

--- Bapad/UnitTests/TestHelpers.h
+++ Bapad/UnitTests/TestHelpers.h
@@ -26,6 +26,12 @@ inline void UniversalPrinter<std::vector<char16_t>>::Print(
 }  // namespace internal
 }  // namespace testing
 
+// gtest 1.8.1 tries to stream char16_t via ostream, which was deleted in C++20.
+inline std::ostream& operator<<(std::ostream& os, char16_t c) {
+  os << static_cast<int>(c);
+  return os;
+}
+
 // Convert a UTF-8 std::string to UTF-16 std::vector<char16_t> for test input
 inline std::vector<char16_t> toWCharVector(const std::string& s) {
   std::vector<char> str(s.begin(), s.end());
